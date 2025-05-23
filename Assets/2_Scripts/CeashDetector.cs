c2bb3338439c9e667f2054a0b623cf81024e325a
shw-jpg <vahyeon2502@sonline20.sen.go.kr>
2025-05-23 15:20:36 +0900

    40번까지 완료

--- Assets/2_Scripts/CeashDetector.cs
+++ Assets/2_Scripts/CeashDetector.cs
@@ -1,22 +1,21 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CeashDetector : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
+    [SerializeField] private float reloadDelay = 2f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("오!,내 머리야!");
+            Invoke(nameof(LoadFirstScene), reloadDelay);
         }
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/2_Scripts/FinishLine.cs
+++ Assets/2_Scripts/FinishLine.cs
@@ -1,22 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class FinishLine : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
+    [Header("씬 전환 대기 시간(초)")]
+    [SerializeField] private float reloaDelay = 2f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Debug.Log("완주했습니다!");
+            Invoke(nameof(LoadFirstScene), reloaDelay);
         }
+    }
+
+    private void LoadFirstScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
